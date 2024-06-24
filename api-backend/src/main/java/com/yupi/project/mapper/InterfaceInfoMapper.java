0eb521d426bfa602b28f17a3cdfdb0b22a8901a8
hyw <hu2004125@163.com>
2024-06-24 12:20:36 +0800

    第一次提交

---
+++ api-backend/src/main/java/com/yupi/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,14 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.yuapicommon.model.entity.InterfaceInfo;
+
+/**
+ * 接口信息 Mapper
+ */
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
